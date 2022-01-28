5966c916ab0290f5e4491ba3b267c662c830ef97
Nicholas Easter <nicholas.easter17@gmail.com>
2022-01-28 13:29:28 -0800

    added comments for how the question flow will be

---
+++ index.js
@@ -0,0 +1,37 @@
+/*Get user input for:
+1. Enter Team Manager's name: 
+2. Enter Team Manager's ID number:
+3. Enter Team Manager's Email address:
+4. Enter Team Manager's Office number:
+
+5. Select option:
+ - Add Engineer
+ - Add Intern
+ - Finish building my team
+
+ ---> If they select Engineer:
+1. Enter Engineer's name: 
+2. Enter Engineer's ID number:
+3. Enter Engineer's Email address:
+4. Enter Engineer's GitHub username:
+
+5. Select option:
+ - Add Engineer
+ - Add Intern
+ - Finish building my team
+
+ ---> If they select Intern
+1. Enter Intern's name: 
+2. Enter Intern's ID number:
+3. Enter Intern's Email address:
+4. Enter Intern's School:
+
+5. Select option:
+ - Add Engineer
+ - Add Intern
+ - Finish building my team
+
+ ---> If they select Finish building my team
+ Exit application and generate HTML file
+
+*/
